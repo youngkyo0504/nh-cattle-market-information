d052577411af3440a5f68d2304a46b8c63cbfaa3
kyo-young <keumky2@gmail.com>
2022-01-25 16:13:14 +0900

    [First Commit] First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,106 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { url } from 'inspector';
+import { URL } from 'url';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav id="header" className="bg-white sticky top-0 w-full z-30  py-1">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
+            <div className="order-1 md:order-1">
+                <a className="flex items-center tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
+                    <svg className="fill-current text-gray-800 mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M5,22h14c1.103,0,2-0.897,2-2V9c0-0.553-0.447-1-1-1h-3V7c0-2.757-2.243-5-5-5S7,4.243,7,7v1H4C3.447,8,3,8.447,3,9v11 C3,21.103,3.897,22,5,22z M9,7c0-1.654,1.346-3,3-3s3,1.346,3,3v1H9V7z M5,10h2v2h2v-2h6v2h2v-2h2l0.002,10H5V10z" />
+                    </svg>
+                    우시장 정보 센터
+                </a>
+            </div>
+            <label htmlFor="menu-toggle" className="cursor-pointer md:hidden block">
+                <svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                    <title>menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                </svg>
+            </label>
+            <input className="hidden" type="checkbox" id="menu-toggle" />
+
+            <div className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-2" id="menu">
+                <nav>
+                    <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
+                        <li><a className="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">서비스 소개</a></li>
+                        <li><a className="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">우시장 둘러보기</a></li>
+                        <li><a className="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">자주 묻는 질문</a></li>
+                        <li><a className="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">우시장 정보</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            
+
+        </div>
+    </nav>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
+        <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
+          <a href="/" className="mb-6 sm:mx-auto">
+            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
+              <svg
+                className="w-10 h-10 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+          </a>
+          <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+              <span className="relative inline-block">
+               
+              </span>{' '}
+                <p>
+                  데이터를 통한 
+                  </p> 
+                  합리적인 송아지 구매
+            </h2>
+            
+          </div>
+          <div>
+            <a
+              href="/"
+              className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+            >
+              Get started
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <div className="bg-sky-300 w-full  mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+        <div className="w-full flex flex-col items-center relative z-10">
+            <h1 className="font-extrabold text-7xl text-center sm:text-8xl text-white leading-tight mt-4">
+                데이터를 통한 합리적인 송아지 구매
+            </h1>
+           
+            <a href="#" className="block bg-gray-300 hover:bg-gray-300 py-3 px-4 text-lg text-white font-bold uppercase mt-10">
+            <button
+            type="button"
+            className="py-3 px-6 text-lg rounded-lg transition-colors duration-200 text-gray-800 hover:text-gray-700 dark:text-gray-200 dark:hover:text-gray-300 hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-800 dark:active:bg-gray-700"
+          >
+            우시장 둘러보기
+          </button>
+            </a>
+        </div>
+    </div>
+    </>
   );
 }
 
